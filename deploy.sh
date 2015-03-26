8fa7e6053071997b9b0bdf689d2b866e56d8d7c9
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-03-26 20:47:00 +0100

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,13 @@ cp CNAME dist/CNAME
 # entire 'dist' content is ready to be deployed
 git branch -D gh-pages
 git checkout -b gh-pages
-ls | grep -v '[dist|node_modules]' | xargs rm -rf
+# gh-pages branch has been dropped and synced with current master
+ls | grep -v -e 'dist' -e 'node_modules' | xargs rm -rf
 git rm $(git ls-files --deleted)
 mv dist/* .
 git add .
 git commit --amend --no-edit
+# push with force and go back to master
 git push -f
 git checkout master
 
